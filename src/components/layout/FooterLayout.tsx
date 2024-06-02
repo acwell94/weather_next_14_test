9468a4e4e1de22ee29fc6a0aa5648d82eea2b175
minyoung <acwell94@naver.com>
2024-06-02 14:50:41 +0900

    footer 메뉴 수정

--- src/components/layout/FooterLayout.tsx
+++ src/components/layout/FooterLayout.tsx
@@ -1,8 +1,28 @@
-export default function FooterLayout(){
-    return(
-        <div>
-            <div>푸터</div>
-            <div>푸터입니까?</div>
+import Image from 'next/image';
+import style from '@/styles/layout/layout.module.css';
+export default function FooterLayout() {
+  return (
+    <nav className={style.footer}>
+      <div className={style.footerWrapper}>
+        <div className={style.footerContainer}>
+          <div className={style.footerIconImgContainer}>
+            <Image src="/icons/footer/today.svg" alt="today" fill />
+          </div>
+          <div className={style.footerContentsName}>Today</div>
         </div>
-    )
+        <div className={style.footerContainer}>
+          <div className={style.footerIconImgContainer}>
+            <Image src="/icons/footer/time.svg" alt="today" fill />
+          </div>
+          <div className={style.footerContentsName}>Hourly</div>
+        </div>
+        <div className={style.footerContainer}>
+          <div className={style.footerIconImgContainer}>
+            <Image src="/icons/footer/calender.svg" alt="today" fill />
+          </div>
+          <div className={style.footerContentsName}>Weekly</div>
+        </div>
+      </div>
+    </nav>
+  );
 }
